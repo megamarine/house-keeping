56f4e10bcd1e3b95e99888e2c9e8925a352b504d
Firman Firdaus <firmanfirdaus542@gmail.com>
2023-11-06 10:07:00 +0700

    update load local again

--- application/views/template_usr/header.php
+++ application/views/template_usr/header.php
@@ -14,7 +14,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="<?= site_url('') ?>assets/template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="<?= site_url('') ?>assets/template/googlefont.css" rel="stylesheet">
+    <link href="<?= site_url('') ?>assets/googlefont.css" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?= site_url('') ?>assets/template/css/sb-admin-2.min.css" rel="stylesheet">
